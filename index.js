d9cf85afd294b52a9136072d3c7526ee8afb7bbf
abdhamid-nasr <142732996+abdhamid-nasr@users.noreply.github.com>
2023-09-19 11:39:45 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+import express from 'express'
+
+import { config } from 'dotenv'
+import path from 'path'
+import { initiateApp } from './src/utils/initiateApp.js'
+config({ path: path.resolve('./config/config.env') })
+
+const app = express()
+
+initiateApp(app, express)
+//cors
